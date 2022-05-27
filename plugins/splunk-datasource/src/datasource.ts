3dddc2bb99693e6b8d49b2ad4b0b7feb02d34f8e
Enrique Fernandez <efcasado@gmail.com>
2022-05-27 16:05:27 +0200

    Close #2

--- plugins/splunk-datasource/src/datasource.ts
+++ plugins/splunk-datasource/src/datasource.ts
@@ -1,7 +1,4 @@
-//import defaults from 'lodash/defaults';
 import { getBackendSrv } from '@grafana/runtime';
-//import https from 'https';
-//import axios from 'axios';
 
 import {
   DataQueryRequest,
@@ -9,7 +6,6 @@ import {
   DataSourceApi,
   DataSourceInstanceSettings,
   MutableDataFrame,
-  //  FieldType,
 } from '@grafana/data';
 
 import { MyQuery, MyDataSourceOptions } from './types';
@@ -31,15 +27,19 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
           fields: [],
         });
 
-        let fields = response.data.fields.map((field: any) => field['name']);
-        fields.forEach((field: any) => {
-          console.log(`DEBUG: field=${field}`);
+        // console.log(`DEBUG: nFields=${response.fields.length}`);
+        // console.log(`DEBUG: nResults=${response.results.length}`);
+
+        //let fields = response.data.fields.map((field: any) => field['name']);
+        response.fields.forEach((field: any) => {
+          // console.log(`DEBUG: field=${field}`);
           frame.addField({ name: field });
         });
 
-        response.data.results.forEach((result: any) => {
+        response.results.forEach((result: any) => {
+          // console.log(`DEBUG: result=${JSON.stringify(result)}`);
           let row: any[] = [];
-          fields.forEach((field: any) => {
+          response.fields.forEach((field: any) => {
             row.push(result[field]);
           });
           frame.appendRow(row);
@@ -87,42 +87,99 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       );
   }
 
-  async doRequest(query: MyQuery, options: DataQueryRequest<MyQuery>) {
-    // https://docs.splunk.com/Documentation/Splunk/8.2.6/Admin/Limitsconf#.5Brestapi.5D
-    // oneshot requests are limited to 100 results
+  async doSearchStatusRequest(sid: string) {
+    const routePath = '/splunk-datasource';
+    const result: boolean = await getBackendSrv()
+      .datasourceRequest({
+        method: 'GET',
+        url: this.url + routePath + '/services/search/jobs/' + sid,
+        params: {
+            output_mode: 'json'
+        }
+      })
+      .then((response) => {
+        let status = response.data.entry[0].content.dispatchState;
+        // console.log(`DEBUG: dispatchState=${status}`);
+        return status === 'DONE' || status === 'PAUSED' || status === 'FAILED';
+      });
+
+    return result;
+  }
+
+  async doSearchRequest(query: MyQuery, options: DataQueryRequest<MyQuery>) {
+    const routePath = '/splunk-datasource';
     const { range } = options;
-    const from = Math.floor(range!.from.valueOf() / 1000); // UNIX time in seconds
-    const to = Math.floor(range!.to.valueOf() / 1000); // UNIX time in seconds
+    const from = Math.floor(range!.from.valueOf() / 1000);
+    const to = Math.floor(range!.to.valueOf() / 1000);
 
     const data = new URLSearchParams({
       search: `search ${query.queryText}`,
       output_mode: 'json',
-      exec_mode: 'oneshot',
       earliest_time: from.toString(),
       latest_time: to.toString(),
     }).toString();
 
-    console.log(`DEBUG: data=${data}`);
+    const sid: string = await getBackendSrv()
+      .datasourceRequest({
+        method: 'POST',
+        url: this.url + routePath + '/services/search/jobs',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data: data,
+      })
+      .then((response) => {
+        return response.data.sid;
+      });
 
-    const routePath = '/splunk-datasource';
+    return sid;
+  }
 
-    console.log(this.url + routePath + '/services/search/jobs');
-    // Convert Grafana's 'from' and 'to' parameters to Splunk's format
-    const result = await getBackendSrv().datasourceRequest({
-      method: 'POST',
-      url: this.url + routePath + '/services/search/jobs',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      data: data,
-    });
+  async doGetAllResultsRequest(sid: string) {
+    const count = 50000;
+    let offset = 0;
+    let isFirst = true;
+    let isFinished = false;
+    let fields: any[] = [];
+    let results: any[] = [];
 
-    // in Splunk: /opt/splunk/etc/system/default/server.conf
-    // crossOriginSharingPolicy = *
-    // crossOriginSharingHeaders = *
-    // in Splunk:
-    // enableSplunkdSSL = false
+    while (!isFinished) {
+        const routePath = '/splunk-datasource';
+        await getBackendSrv().datasourceRequest({
+            method: 'GET',
+            url: this.url + routePath + '/services/search/jobs/' + sid + '/results',
+            params: {
+                output_mode: 'json',
+                offset: offset,
+                count: count
+            }
+        }).then((response) => {
+            // console.log(`DEBUG: count=${count} offset=${offset} ${JSON.stringify(response.data)}`);
+            if (response.data.post_process_count === 0 && response.data.results.length === 0) {
+                isFinished = true;
+            } else {
+                if (isFirst) {
+                    isFirst = false;
+                    fields = response.data.fields.map((field: any) => field['name']);
+                }
+                offset = offset + count;
+                results = results.concat(response.data.results);
+            }
+        });
 
+        offset = offset + count;
+    }
+
+      return { fields: fields, results: results };
+  }
+
+  async doRequest(query: MyQuery, options: DataQueryRequest<MyQuery>) {
+    const sid: string = await this.doSearchRequest(query, options);
+    // console.log(`DEBUG: sid=${sid}`);
+
+    while (!await this.doSearchStatusRequest(sid)) {}
+
+    const result = await this.doGetAllResultsRequest(sid);
     return result;
   }
 }
